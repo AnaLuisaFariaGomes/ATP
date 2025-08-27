8477416844e7b1b3e866bade6851b07a0ce219df
Ana Luísa <129781832+AnaLuisaFariaGomes@users.noreply.github.com>
2025-08-27 16:55:10 -0300

    10.cs

---
+++ Lista2/10.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex10
+{
+    internal class _10
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Escreva um número entre 0 e 4: ");
+            num = int.Parse(Console.ReadLine());
+
+            switch (num)
+            {
+                case 0:
+                    Console.WriteLine("Zero");
+                    break;
+                case 1:
+                    Console.WriteLine("Um");
+                    break;
+                case 2:
+                    Console.WriteLine("Dois");
+                    break;
+                case 3:
+                    Console.WriteLine("Três");
+                    break;
+                case 4:
+                    Console.WriteLine("Quatro");
+                    break;
+                default:
+                    Console.WriteLine("Valor inválido.");
+                    break;
+            }
+        }
+    }
+}
