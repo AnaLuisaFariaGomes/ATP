41241d43b8a50e35053674715ccb1b6966b4e3ee
Ana Luísa <129781832+AnaLuisaFariaGomes@users.noreply.github.com>
2025-09-01 12:15:40 -0300

    01.cs

---
+++ Lista3/01.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3
+{
+    internal class _01
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+
+            while (num > 0)
+            {
+                Console.WriteLine(num);
+                num--;
+            }
+            Console.WriteLine("Feliz ano novo!");
+        }
+    }
+}
