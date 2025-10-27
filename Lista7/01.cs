b74986ef9cd330c104d65877caa7119e106a0334
Ana Luísa <129781832+AnaLuisaFariaGomes@users.noreply.github.com>
2025-10-27 11:11:59 -0300

    Implementação método para descobrir o Menor entre 4 int
    Correção de lógica

--- Lista7/01.cs
+++ Lista7/01.cs
@@ -2,15 +2,15 @@
  {
      static int Menor(int n1, int n2, int n3, int n4)
      {
-         if (n1 < n2 && n1 < n3 && n1 < n4)
+         if (n1 =< n2 && n1 =< n3 && n1 =< n4)
          {
              return n1;
          }
-         else if (n2 < n1 && n2 < n3 && n2 < n4)
+         else if (n2 =< n1 && n2 =< n3 && n2 =< n4)
          {
              return n2;
          }
-         else if (n3 < n2 && n3 < n1 && n3 < n4)
+         else if (n3 =< n2 && n3 =< n1 && n3 =< n4)
          {
              return n3;
          }
@@ -23,11 +23,11 @@
      {
          Console.WriteLine("Digite o valor de n1: ");
          int n1 = int.Parse(Console.ReadLine());
-         Console.WriteLine("Digite o valor de n1: ");
+         Console.WriteLine("Digite o valor de n2: ");
          int n2 = int.Parse(Console.ReadLine());
-         Console.WriteLine("Digite o valor de n1: ");
+         Console.WriteLine("Digite o valor de n3: ");
          int n3 = int.Parse(Console.ReadLine());
-         Console.WriteLine("Digite o valor de n1: ");
+         Console.WriteLine("Digite o valor de n4: ");
          int n4 = int.Parse(Console.ReadLine());
 
          int m = Menor(n1, n2, n3, n4);
