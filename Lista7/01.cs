5faa6a9b44ea3e89097e60f77ccaa431be46da36
Ana Luísa <129781832+AnaLuisaFariaGomes@users.noreply.github.com>
2025-10-27 11:06:24 -0300

    Implementação método para descobrir o Menor entre 4 int

---
+++ Lista7/01.cs
@@ -0,0 +1,35 @@
+ internal class _01
+ {
+     static int Menor(int n1, int n2, int n3, int n4)
+     {
+         if (n1 < n2 && n1 < n3 && n1 < n4)
+         {
+             return n1;
+         }
+         else if (n2 < n1 && n2 < n3 && n2 < n4)
+         {
+             return n2;
+         }
+         else if (n3 < n2 && n3 < n1 && n3 < n4)
+         {
+             return n3;
+         }
+         else
+         {
+             return n4;
+         }
+     }
+     static void Main(string[] args)
+     {
+         Console.WriteLine("Digite o valor de n1: ");
+         int n1 = int.Parse(Console.ReadLine());
+         Console.WriteLine("Digite o valor de n1: ");
+         int n2 = int.Parse(Console.ReadLine());
+         Console.WriteLine("Digite o valor de n1: ");
+         int n3 = int.Parse(Console.ReadLine());
+         Console.WriteLine("Digite o valor de n1: ");
+         int n4 = int.Parse(Console.ReadLine());
+
+         int m = Menor(n1, n2, n3, n4);
+         Console.WriteLine($"O menor número entre eles é {m}.");
+     }
