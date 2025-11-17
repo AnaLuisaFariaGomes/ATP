9e7b17eb58324dca09e30615016d6012f8ebbfa8
Ana Luísa <129781832+AnaLuisaFariaGomes@users.noreply.github.com>
2025-11-17 11:38:05 -0300

    Aplicação da classe Data na classe Teste método Main

---
+++ Lista09 (Classes)/Ex02/Teste.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    internal class Teste
+    {
+        static void Main(string[] args)
+        {
+            Data d1 = new Data(17, 11, 2025);
+            Console.WriteLine(d1);
+            Data d2 = new Data(25, 12, 2025);
+            Console.WriteLine(d2);
+        }
+    }
+}
